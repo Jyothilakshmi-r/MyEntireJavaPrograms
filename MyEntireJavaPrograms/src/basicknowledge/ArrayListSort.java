bb63b0b74977f9c15739530277229ffae0a7fc23
jyothilr83 <jyothilr83@gmail.com>
2024-05-30 00:50:42 -0400

    Arraylistsort commit done

---
+++ MyEntireJavaPrograms/src/basicknowledge/ArrayListSort.java
@@ -0,0 +1,31 @@
+package CollectionEg;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListSort {
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> arrList = new ArrayList<>();
+        arrList.add(22);
+        arrList.add(51);
+        arrList.add(45);
+        arrList.add(91);
+        arrList.add(1);
+        
+        System.out.println("Elements of integer arraylist before sorting: " + arrList);
+        Collections.sort(arrList);
+        System.out.println("Sorted elements of integer arraylist: " + arrList);
+        
+        
+        ArrayList<String> strList = new ArrayList<>();
+        strList.add("Jyothi");
+        strList.add("Aju");
+        strList.add("Vedha");
+        strList.add("Nandha");
+        strList.add("Deva");
+        
+        System.out.println("Elements of string arraylist before sorting: " + strList);
+        Collections.sort(strList);
+        System.out.println("Sorted elements of string arrayist: " + strList);
+    }
+}
